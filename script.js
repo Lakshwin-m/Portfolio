475a23197049eee1736b05e1d3edad9ca834e731
Lakshwin <58813436+Lakshwin-m@users.noreply.github.com>
2024-09-24 20:19:45 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+function toggleMenu() {
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".hamburger-icon");
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+  }
+  function typeWriter(element, text, speed = 100) {
+    let i = 0;
+    function type() {
+        if (i < text.length) {
+            element.innerHTML += text.charAt(i);
+            i++;
+            setTimeout(type, speed);
+        }
+    }
+    type();
+}
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    const nameElement = document.getElementById('name');
+    
+
+    typeWriter(nameElement, 'Lakshwin Krishna', 100);
+   
+});
