a979ea905d448f0d044a29e36ed791f7be3a4a3b
Lakshwin <58813436+Lakshwin-m@users.noreply.github.com>
2024-10-16 20:31:48 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
-export default defineConfig({https://github.com/Lakshwin-m/Portfolio/blob/main/vite.config.js
+export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
-      external: ['framer-motion','react-icons/fa','react-icons/gi','react-icons/ri','react-icons/io5','react-icons/si'], // Add 'framer-motion' as an external module
+      external: [
+        'framer-motion',
+        'react-icons/fa',
+        'react-icons/gi',
+        'react-icons/ri',
+        'react-icons/io5',
+        'react-icons/si'
+      ], // Add specified modules as external
     },
   },
-})
+});
